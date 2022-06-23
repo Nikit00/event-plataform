daee627d6cfc91d4b75d2268c6cf35720500ca4b
Henrique Rosa <89054782+Nikit00@users.noreply.github.com>
2022-06-22 23:37:00 -0300

    Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { Sidebar } from "./components/Sidebar"
-import { Video } from "./components/Video"
-import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   
   return (
       <div>
-        <Event />
-        
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ApolloProvider>
       </div>
   )
 }
