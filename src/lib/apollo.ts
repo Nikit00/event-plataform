6b48f37e0ba42465023574e2cd5f449f505417e2
Henrique Rosa <89054782+Nikit00@users.noreply.github.com>
2022-06-24 00:35:13 -0300

    Aula 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8cymr17fx01xm9n6fh8r3/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
